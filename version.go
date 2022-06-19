8b83a77b4e1178b5415bfbc45368f8a3785c9bf0
Zlatko Čalušić <zcalusic@bitsync.net>
2022-06-19 19:17:14 +0200

    Version 1.0.0

---
+++ version.go
@@ -0,0 +1,8 @@
+// Copyright © 2022 Zlatko Čalušić
+//
+// Use of this source code is governed by an MIT-style license that can be found in the LICENSE file.
+
+package main
+
+// Version of the temp utility.
+const Version = "1.0.0"
